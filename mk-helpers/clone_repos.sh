c7bf774d2b12b720415e1f9a9cc669d9a286ed29
Dmitry <mich.dima@gmail.com>
2017-04-20 12:38:41 +0300

    Upgrade

--- mk-helpers/clone_repos.sh
+++ mk-helpers/clone_repos.sh
@@ -1,30 +1,13 @@
 #!/usr/bin/env bash
 # This script clone blog repos to parent_dir/module5_app
 
-source data/creds.env
 source mk-helpers/env.vars
-eval $(docker-machine env $machine)
+#
+DIR=module5_app
+mkdir $DIR
+cd $DIR
 
-DIR="module5_app"
-mkdir ../$DIR
-cd ../$DIR
-
-for i in ubuntu ruby mongodb blog_ui blog_backend blog; do
-  printf "Trying to clone $i repo\n"
+printf "clone repo"
+for i in docker ubuntu ruby mongodb blog_ui blog_backend blog; do
   git clone http://$GITLAB_USER:$GITLAB_PASSWORD@$module5_host/module5/$i.git;
-done
-
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
-   find . -type f -name Dockerfile -exec sed -i "s/changeme/$DOCKERHUB_USER/" {} +
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-   find . -type f -name Dockerfile -exec sed -i '' s/changeme/$DOCKERHUB_USER/ {} +
-fi
-
-echo "Change dir to $DIR for working with Gitlab repo"
-
-for i in ubuntu ruby mongodb blog_ui blog_backend blog; do
-  printf "Push updated changes"
-  cd $i; git commit -am "Up"; \
-  git push origin master; \
-  cd ../
 done
