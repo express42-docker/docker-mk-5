2a28df8a098e860da715c1cf054317287ebbcf07
Sergey Bogatyrets <serj.sj@gmail.com>
2017-05-18 16:05:17 +0300

    Extra fancy preseed. Fix docker network for gitlab runner

--- mk-helpers/preseed.sh
+++ mk-helpers/preseed.sh
@@ -1,74 +1,68 @@
 #!/usr/bin/env bash
-# This script create docker-machine with Gitlab.
-# Without parameters start Docker host in Amazon. With v - in Virtualbox, p - in Parallels.
+# This script create docker-machine with Gitlab infrastructure
 
-# Creating machine with remote provider
 source mk-helpers/env.vars
 provision() {
-
-  if [[ ! $(docker-machine status $machine) = "Running" ]]; then
-    printf "\nСоздаем сервер с Gitlab CI\n"
+  if [ `docker-machine status $machine &> /dev/null; echo $?` -ne 0 ]; then
+    printf "Создаем сервер с Gitlab CI\n"
     case $1 in
       '-p' ) docker-machine create -d parallels --parallels-cpu-count 4 --parallels-disk-size "26000" --parallels-memory "4048" $machine;;
       '-v' ) docker-machine create -d virtualbox --virtualbox-cpu-count 4 --virtualbox-disk-size "26000" --virtualbox-memory "4048" $machine;;
       * ) docker-machine create -d amazonec2 --amazonec2-root-size "12" --amazonec2-instance-type "t2.medium" --amazonec2-region "eu-central-1" --amazonec2-subnet-id "subnet-ccbf57a5" $machine;;
     esac
   fi
-    # Set machine vars
-    source mk-helpers/env.vars
-    # Setup infrastructure
-    docker-compose up -d
 
-    printf "\n\n\nСоздаем пользователя и обновляем регистрацию\n"
+  # Set machine vars
+  source mk-helpers/env.vars
+  # Setup infrastructure
+  docker-compose up -d
 
+  printf "\nСоздаем пользователя и обновляем регистрацию"
+  while [ $(curl --write-out %{http_code} --silent --output /dev/null http://$module5_host/users/sign_in) -ne 200 ]; do
+    printf "."
+    # Убираем возможность регистрации на время мастер-класса
+    docker-compose exec -T gitlab gitlab-rails runner "ApplicationSetting.last.update_attributes(signup_enabled: false)" > /dev/null
 
-    while [ $(curl --write-out %{http_code} --silent --output /dev/null http://$module5_host/users/sign_in) -ne 200 ]; do
-      # Убираем возможность регистрации на время мастер-класса
-      docker-compose exec -T gitlab gitlab-rails runner "ApplicationSetting.last.update_attributes(signup_enabled: false)" > /dev/null
+    # Создаем пользователя
+    docker-compose exec -T gitlab gitlab-rails runner "user = User.find_by(email: 'admin@example.com'); user.password = \"$GITLAB_PASSWORD\"; user.password_confirmation = \"$GITLAB_PASSWORD\"; user.password_automatically_set = false; user.save" > /dev/null
+  done
 
-      # Создаем пользователя
-      docker-compose exec -T gitlab gitlab-rails runner "user = User.find_by(email: 'admin@example.com'); user.password = \"$GITLAB_PASSWORD\"; user.password_confirmation = \"$GITLAB_PASSWORD\"; user.password_automatically_set = false; user.save" > /dev/null
-    done
+  printf "\nГотовим окружение к использованию локального docker registry\n"
+  #####  Prepare environment for local registry
+  if [ `docker-machine ssh $machine "ping $DOCKER_REGISTRY -c 1" &> /dev/null; echo $?` -ne 0 ]; then
+    docker-machine ssh $machine "sudo /bin/sh -c 'echo ${module5_host} $DOCKER_REGISTRY >> /etc/hosts'"
+  fi
 
-    printf "\n\n\nГотовим окружение к использованию  локального docker registry\n"
-    #####  Prepare environment for local registry
-    if ! docker-machine ssh $machine "ping $DOCKER_REGISTRY -c 1 &>  /dev/null" ; then
-      docker-machine ssh $machine "sudo /bin/sh -c 'echo ${module5_host} $DOCKER_REGISTRY >> /etc/hosts'"
-    fi
+  if [ `docker-machine ssh $machine "sudo test -e /etc/docker/daemon.json" &> /dev/null; echo $?` -ne 0 ]; then
+    docker-machine ssh $machine "echo {\'insecure-registries\':[\'$DOCKER_REGISTRY\']} |  tr \"'\" '\"' | sudo  tee /etc/docker/daemon.json"
+    docker-machine ssh $machine sudo /etc/init.d/docker restart
+  fi
 
-    if ! docker-machine ssh $machine "sudo test -e /etc/docker/daemon.json"  ; then
-      docker-machine ssh $machine "echo {\'insecure-registries\':[\'$DOCKER_REGISTRY\']} |  tr \"'\" '\"' | sudo  tee /etc/docker/daemon.json"
-      docker-machine ssh $machine sudo /etc/init.d/docker restart
-    fi
-      file=/srv/docker/gitlab/config/gitlab.rb
-
-      docker-machine ssh $machine "sudo sed -i 's/.*registry_external_url.*/registry_external_url \"http:\/\/$DOCKER_REGISTRY\"/' $file "
-      docker-machine ssh $machine "sudo sed -i '/^#.*registry_enabled/s/^#//' $file"
-      docker-machine ssh $machine "sudo sed -i '/^#.*registry\[.enable.\]/s/^#//' $file"
-      docker-compose restart gitlab
+  file=/srv/docker/gitlab/config/gitlab.rb
+  docker-machine ssh $machine "sudo sed -i 's/.*registry_external_url.*/registry_external_url \"http:\/\/$DOCKER_REGISTRY\"/' $file "
+  docker-machine ssh $machine "sudo sed -i '/^#.*registry_enabled/s/^#//' $file"
+  docker-machine ssh $machine "sudo sed -i '/^#.*registry\[.enable.\]/s/^#//' $file"
+  docker-compose restart gitlab
 }
 
 seed() {
-
   BACKUP_FILE=${BACKUP}_gitlab_backup.tar
 
-  printf "Подготовка проектов\n"
-  docker-machine scp mk-helpers/$BACKUP_FILE $machine:~/
+  printf "\nПодготовка проектов\n"
+  docker-machine scp data/$BACKUP_FILE $machine:~/
   docker-machine ssh $machine "sudo cp ~/$BACKUP_FILE /srv/docker/gitlab/data/backups/"
 
-  # docker-compose exec gitlab wget https://s3.eu-central-1.amazonaws.com/docker-mk-mar-2017/module5/$BACKUP_FILE -P /var/opt/gitlab/backups/
-
-  printf "Импортируем проекты\n"
-  docker-compose exec gitlab sh -c "yes yes | /opt/gitlab/bin/gitlab-rake gitlab:backup:restore BACKUP=$BACKUP"
+  printf "\nИмпортируем проекты\n"
+  docker-compose exec gitlab sh -c "yes yes | /opt/gitlab/bin/gitlab-rake gitlab:backup:restore BACKUP=$BACKUP" > /dev/null
 
   ## Add variables
   printf "\nАктуализируем конфигурацию CI"
   for i in `seq 10 16`;
   do
+      printf "."
       docker-compose exec -T gitlab gitlab-rails runner "\
       Ci::Variable.create :key => \"DEV_HOST\", :value => \"$module5_host\", :project_id => $i;
-      Ci::Variable.create :key => \"BUILD_TOKEN\", :value => \"$BUILD_TOKEN\", :project_id => $i; "
-      printf "."
+      Ci::Variable.create :key => \"BUILD_TOKEN\", :value => \"$BUILD_TOKEN\", :project_id => $i;"
 
   done
 
@@ -78,7 +72,6 @@ seed() {
 # Create gitlab CI images
 
 create_images() {
-
   SRC_CERTS=~/.docker/machine/machines/$machine/*.pem
   DST_CERTS=images/docker-git-compose/certs
 
@@ -131,15 +124,15 @@ case $1 in
     runner
     ;;
   * ) printf  "\
-        Usage:
-      --provision - provision VM and deploy gitlab on it
-      --seed - seed projects from backup
-      --create_images - create base docker images for ci builds and push them to registry
-      --runner - reconfigure gitlab ci runner
-      --full - fulfill all presented stages
-      -p - along with --provision used to up parallels env
-      -v - along with --provision used to up virtualbox env
-      \n"
+      Usage:
+    --provision - provision VM and deploy gitlab on it
+    --seed - seed projects from backup
+    --create_images - create base docker images for ci builds and push them to registry
+    --runner - reconfigure gitlab ci runner
+    --full - fulfill all presented stages
+    -p - along with --provision used to up parallels env
+    -v - along with --provision used to up virtualbox env
+    \n"
 esac
 
 printf "\nЖдём загрузки Gitlab\n"
